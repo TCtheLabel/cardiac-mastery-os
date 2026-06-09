377a2835f1459e28938272d2187c77458881a702
TCtheLabel <colemanth@yahoo.com>
2026-06-08 21:14:02 -0500

    Set up Vitest test runner

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(({ mode }) => ({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "node",
+    globals: true,
+    env: loadEnv(mode, process.cwd(), ""),
+  },
+}));
